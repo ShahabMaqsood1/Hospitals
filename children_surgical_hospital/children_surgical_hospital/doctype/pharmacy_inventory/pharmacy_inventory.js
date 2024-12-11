519f3be59b182a24f4175c6a8409a1414ad59fbc
ShahabMaqsood1 <Randeljack123@gmail.com>
2024-12-11 15:33:46 +0000

    first commit

---
+++ children_surgical_hospital/children_surgical_hospital/doctype/pharmacy_inventory/pharmacy_inventory.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Shahab Maqsood and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Pharmacy Inventory", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ children_surgical_hospital/children_surgical_hospital/doctype/pharmacy_sales/test_pharmacy_sales.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Shahab Maqsood and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPharmacySales(FrappeTestCase):
+	pass
