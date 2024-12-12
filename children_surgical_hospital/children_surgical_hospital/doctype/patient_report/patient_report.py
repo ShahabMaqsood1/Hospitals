d501f4298439f2071120321da2c7f26aa4995122
FarhanAtique <farhanatique44@gmail.com>
2024-12-12 12:07:48 +0000

    first commit

---
+++ children_surgical_hospital/children_surgical_hospital/doctype/patient_report/patient_report.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Shahab Maqsood and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PatientReport(Document):
+	pass
