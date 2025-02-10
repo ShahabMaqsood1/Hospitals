0a32ee2c6263baf8d7185fbcb4d1a754b89687eb
ShahabMaqsood1 <randeljack123@gmail.com>
2025-02-10 12:27:49 +0000

    first commit

--- children_surgical_hospital/children_surgical_hospital/doctype/medicine_names/medicine_names.py
+++ children_surgical_hospital/children_surgical_hospital/doctype/medicine_names/medicine_names.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class Medicine(Document):
+class MedicineNames(Document):
 	pass
--- children_surgical_hospital/children_surgical_hospital/doctype/presenting_complaints/test_presenting_complaints.py
+++ children_surgical_hospital/children_surgical_hospital/doctype/presenting_complaints/test_presenting_complaints.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestMedicine(FrappeTestCase):
+class TestPresentingComplaints(FrappeTestCase):
 	pass
