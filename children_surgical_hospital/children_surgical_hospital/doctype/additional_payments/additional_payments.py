f773f4ed7aaa1ec39e946fbed5d3975292984ac8
Shahab Maqsood <146881392+ShahabMaqsood1@users.noreply.github.com>
2025-02-18 01:03:28 +0500

    Update additional_payments.py

--- children_surgical_hospital/children_surgical_hospital/doctype/additional_payments/additional_payments.py
+++ children_surgical_hospital/children_surgical_hospital/doctype/additional_payments/additional_payments.py
@@ -1,7 +1,7 @@
 import frappe
 from frappe.model.document import Document
 
-class PatientAppointment(Document):
+class AdditionalPayments(Document):
     def before_insert(self):
         if not self.receptionist:
             user_full_name = frappe.db.get_value("User", frappe.session.user, "full_name")
