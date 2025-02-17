7ac7eadc62b29f3118390a00307536e023d62534
Shahab Maqsood <146881392+ShahabMaqsood1@users.noreply.github.com>
2025-02-18 00:08:54 +0500

    Delete children_surgical_hospital/children_surgical_hospital/children_surgical_hospital directory

--- children_surgical_hospital/children_surgical_hospital/children_surgical_hospital/doctype/pharmacy_inventory/pharmacy_inventory.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Shahab Maqsood and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Pharmacy Inventory", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- children_surgical_hospital/children_surgical_hospital/children_surgical_hospital/report/check_patient_report/check_patient_report.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Shahab Maqsood and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Check Patient Report"] = {
-	"filters": [
-
-	]
-};
